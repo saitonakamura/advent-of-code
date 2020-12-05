c84f83d31e4026db910ac930a8163a8df85c868c
Michael Bashurov <accounts@saitonakamura.me>
2020-12-05 15:25:03 +0200

    day5 fp

--- advent2020/day5.ts
+++ advent2020/day5.ts
@@ -1,59 +1,47 @@
-import { setPriority } from "os";
+import { map, reduce } from "fp-ts/lib/Array";
+import { flow } from "fp-ts/lib/function";
 import { read } from "./helper";
 
 const parseStr = (s: string) => s;
 
 type T = ReturnType<typeof parseStr>;
 
-const binary = (arr: string, length: number) => {
-  // console.log(arr);
-  return (
-    [...arr].reduce(
-      ({ value, step }, curr) => {
-        // console.log({ curr, value, step });
-        return curr === "F" || curr === "L"
-          ? { value: value - step, step: step / 2 }
-          : { value: value + step, step: step / 2 };
-      },
-      { value: length / 2, step: length / 4 }
-    ).value - 0.5
-  );
-};
+const binary = (arr: string, length: number) =>
+  reduce({ value: length / 2, step: length / 4 }, ({ value, step }, curr) => {
+    return curr === "F" || curr === "L"
+      ? { value: value - step, step: step / 2 }
+      : { value: value + step, step: step / 2 };
+  })([...arr]).value - 0.5;
 
-const f1 = (arr: T[]) => {
-  return arr
-    .map((el) => [
-      binary(el.substring(0, 7), 128),
-      binary(el.substring(7, 10), 8),
-    ])
-    .map(([seat, row]) => seat * 8 + row)
-    .reduce((acc, curr) => (acc < curr ? curr : acc), -1);
-};
+const max = reduce(-1, (acc, curr: number) => (acc < curr ? curr : acc));
+
+const min = reduce(Number.MAX_SAFE_INTEGER, (acc, curr: number) =>
+  acc > curr ? curr : acc
+);
+
+const sum = reduce(0, (acc, curr: number) => acc + curr);
+
+const getIds = map(
+  (el: string) =>
+    binary(el.substring(0, 7), 128) * 8 + binary(el.substring(7, 10), 8)
+);
+
+const f1 = flow(getIds, max);
 
 const f2 = (arr: T[]) => {
-  const ids = arr
-    .map((el) => [
-      binary(el.substring(0, 7), 128),
-      binary(el.substring(7, 10), 8),
-    ])
-    .map(([seat, row]) => seat * 8 + row)
-    .sort((a, b) => a - b);
+  const ids = getIds(arr);
+  const ma = max(ids);
+  const mi = min(ids);
+  const realSum = sum(ids);
 
-  let i = 0;
-  let s = ids[i];
+  const fullSum = ((ma + mi) * (ma - mi + 1)) / 2;
 
-  while (s === ids[i]) {
-    s++;
-    i++;
-  }
-
-  return s;
+  return fullSum - realSum;
 };
 
 (async () => {
-  console.log(f1((await read(5, 1)).map(parseStr)));
-  // console.log(f2((await read(5, 1)).map(parseStr)));
-
-  console.log(f1((await read(5, 2)).map(parseStr)));
-  console.log(f2((await read(5, 2)).map(parseStr)));
+  console.log(f1(await read(5, 1)));
+  console.log("\n");
+  console.log(f1(await read(5, 2)));
+  console.log(f2(await read(5, 2)));
 })();
